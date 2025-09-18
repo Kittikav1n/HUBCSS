c5ffc2fef173ecc50c513c82e53c971f60586168
KIITIZ_ <kittikavin1998@gmail.com>
2025-09-18 12:15:02 +0700

    lab4

--- CSS111/LAB4/1_Function for Multiplication/code.py
+++ CSS111/LAB4/1_Function for Multiplication/code.py
@@ -3,5 +3,4 @@ def multiplication_table(rows, cols):
         for j in range(1, cols+1):
             print(f"{i*j:<4}", end="")
         print()
-
 multiplication_table(10, 15)
--- CSS111/LAB4/2_Function with List/code.py
+++ CSS111/LAB4/2_Function with List/code.py
@@ -3,16 +3,13 @@ def calculate(weights, max_scores, scores):
     if not weights:
         weights100 = 100 / len(scores)
         weights = [weights100] * len(scores)
-
     for i in range(len(scores)):
         ratio = scores[i] / max_scores[i]
         weights_part = ratio * weights[i]
         total += weights_part
     return round(total, 2)
-
 weights = list(map(int, input("weights: ").split()))
 max_scores = list(map(int, input("max_scores: ").split()))
 scores = list(map(int, input("scores: ").split()))
-
 final_scores = calculate(weights, max_scores, scores)
 print(f"Final weighted score is {final_scores}")
