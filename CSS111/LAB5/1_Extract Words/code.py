b9f9b10ba5065a6288ae73771eba3295ac9b305a
KIITIZ_ <kittikavin1998@gmail.com>
2025-09-25 19:36:40 +0700

    LAB5

---
+++ CSS111/LAB5/1_Extract Words/code.py
@@ -0,0 +1,10 @@
+def extract_words(text):
+    words_list = text.split()
+
+    long_words = [] 
+    for word in words_list:
+        if len(word) > 4:
+            long_words.append(word)
+    return long_words
+
+print(extract_words(input("ENTER TEXT: ")))
---
+++ CSS111/LAB5/2_Password Strength/code.py
@@ -0,0 +1,11 @@
+def strength(passwords):
+    strength_list = []
+    for passwords1 in passwords:
+        if len(passwords1) < 8 or passwords1.isdigit():
+            strength_list.append("weak")
+        elif len(passwords1) >= 12:
+            strength_list.append("strong")
+        else:
+            strength_list.append("Ok")
+    return strength_list
+print(strength(input("ENTER PASSWORDS: ").split()))
