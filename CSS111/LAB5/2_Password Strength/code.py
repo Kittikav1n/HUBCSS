93e13f863b50ee999e2a5cdd576df598fe431a93
KIITIZ_ <kittikavin1998@gmail.com>
2025-09-25 21:20:13 +0700

    lab5

--- CSS111/LAB5/2_Password Strength/code.py
+++ CSS111/LAB5/2_Password Strength/code.py
@@ -1,13 +1,14 @@
 def strength(passwords):
-    strength_list = []
 
+    strength_list = []
     for passwords1 in passwords:
         if len(passwords1) < 8 or passwords1.isdigit():
             strength_list.append("weak")
-            
+
         elif len(passwords1) >= 12:
             strength_list.append("strong")
         else:
             strength_list.append("Ok")
     return strength_list
+
 print(strength(input("ENTER PASSWORDS: ").split()))
