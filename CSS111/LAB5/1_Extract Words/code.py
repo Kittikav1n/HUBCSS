a1e1880022dbd6c4cb498db025a3b251cab5ec69
KIITIZ_ <kittikavin1998@gmail.com>
2025-09-26 13:24:46 +0700

    short code

--- CSS111/LAB5/1_Extract Words/code.py
+++ CSS111/LAB5/1_Extract Words/code.py
@@ -1,7 +1,11 @@
 def extract_words(text):
-    
-    words_list = text.split(" ")
-    long_words = [word for word in words_list if len(word) >= 4]
-    return long_words
+    return [word for word in text.split() if len(word) >= 4]    
 
-print(extract_words(input("ENTER TEXT: ")))
+    #words_list = text.split(" ")
+    #long_words = [word for word in words_list if len(word) >= 4]
+    #return long_words
+
+#print(extract_words(input("ENTER TEXT: ")))
+print(extract_words("Don't judge a book by its cover."))
+print(extract_words("All that glitters is not gold."))
+print(extract_words("The multinational corporation's decentralization strategy required comprehensive reorganization of their compartmentalized departmentalization systems across intercontinental subsidiaries."))
