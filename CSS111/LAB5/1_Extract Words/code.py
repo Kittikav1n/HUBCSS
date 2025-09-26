d71200ee2cda57c4821d486f35d4c59e2e855adb
KIITIZ_ <kittikavin1998@gmail.com>
2025-09-26 12:58:18 +0700

    list comprehension

--- CSS111/LAB5/1_Extract Words/code.py
+++ CSS111/LAB5/1_Extract Words/code.py
@@ -1,9 +1,7 @@
 def extract_words(text):
+    
     words_list = text.split(" ")
-    long_words = [] 
-    for word in words_list:
-        if len(word) > 4:
-            long_words.append(word)
+    long_words = [word for word in words_list if len(word) >= 4]
     return long_words
 
 print(extract_words(input("ENTER TEXT: ")))
--- CSS111/LAB5/2_Password Strength/code.py
+++ CSS111/LAB5/2_Password Strength/code.py
@@ -1,14 +1,23 @@
 def strength(passwords):
 
-    strength_list = []
-    for passwords1 in passwords:
-        if len(passwords1) < 8 or passwords1.isdigit():
-            strength_list.append("weak")
+    strength_list = [
+        "weak" if len(passwords1) < 8 or passwords1.isalpha()
 
-        elif len(passwords1) >= 12:
-            strength_list.append("strong")
-        else:
-            strength_list.append("Ok")
+        else "strong" if len(passwords1) >= 12 
+        and any(char.isalnum() for char in passwords1) 
+        and any(not char.isalnum() for char in passwords1)
+        
+        else "Ok"
+        
+        for passwords1 in passwords
+    ]
     return strength_list
 
-print(strength(input("ENTER PASSWORDS: ").split()))
+
+#print(strength(input("ENTER PASSWORDS: ").split()))
+
+print(strength(["abc", "School2025", "L3arn!ngAI2025"]))
+print(strength(["helloworld", "PythonRocks"]))
+print(strength(["abc12345", "Password1", "Hello2025"]))
+print(strength([""]))
+print(strength(["onlyletters", "Mix123", "GoodOne2023!", "Ultra$strongP@ssw0rd2025"]))
