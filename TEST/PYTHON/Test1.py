c1067f8100890b0c50feb1e3086bd23d303c867d
KIITIZ_ <kittikavin1998@gmail.com>
2025-10-10 21:05:38 +0700

    palindrome

--- TEST/PYTHON/Test1.py
+++ TEST/PYTHON/Test1.py
@@ -6,3 +6,11 @@ print(f"total =  {sum1([1, 2, 3, 4, 5, 6])}")
 print(f"total =  {sum1([10, 21, 33, 44])}")
 print(f"total =  {sum1([1, 3, 5])}")
 print(f"total =  {sum1([])}")
+#palindrome
+def is_palindrome(text):
+    s = " ".join(filter(str.isalnum, text)).lower()
+    return s == s[::-1]
+print(f"ผลลัพธ์: {is_palindrome('A man a plan a canal Panama')}")
+print(f"ผลลัพธ์: {is_palindrome('hello')}")
+print(f"ผลลัพธ์: {is_palindrome('Racecar')}")
+print(f"ผลลัพธ์: {is_palindrome('No lemon, no melon.')}")
