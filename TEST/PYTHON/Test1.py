53f28ddbd06d11f9217f10b4435444c781da1c13
KIITIZ_ <kittikavin1998@gmail.com>
2025-10-10 22:17:59 +0700

    countvowels

--- TEST/PYTHON/Test1.py
+++ TEST/PYTHON/Test1.py
@@ -14,3 +14,16 @@ print(f"ผลลัพธ์: {is_palindrome('A man a plan a canal Panama')}")
 print(f"ผลลัพธ์: {is_palindrome('hello')}")
 print(f"ผลลัพธ์: {is_palindrome('Racecar')}")
 print(f"ผลลัพธ์: {is_palindrome('No lemon, no melon.')}")
+
+#countvowels
+def countvowels(text):
+    s = "".join(filter(str.isalnum, text)).lower()
+    count = 0;
+    for char in s :
+        if char in "aeiou":
+            count = count +1
+    return count;
+print(f"ผลลัพธ์: {countvowels('Hello World')}")
+print(f"ผลลัพธ์: {countvowels('Python Programming')}")
+print(f"ผลลัพธ์: {countvowels('AEIOUaeiou')}")
+print(f"ผลลัพธ์: {countvowels('Rhythm')}")
